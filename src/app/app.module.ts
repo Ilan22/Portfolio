593ad1cb809ad0b6e9a15f79e33340d11c45f695
Ilan22 <ilanlicha22@gmail.com>
2023-02-26 22:18:17 +0100

    feat: sky ground sun clouds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeBackgroundComponent } from './home-background/home-background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeBackgroundComponent
   ],
   imports: [
     BrowserModule,
